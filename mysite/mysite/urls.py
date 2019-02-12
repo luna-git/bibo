0228ad4f8c171c7530ca62d521309905a9b20c1a
luna-git <g.rossi.ar@gmail.com>
2019-02-12 17:17:36 +0100

    Ho modificato la prima pagina

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
 ]
+# from django.conf.urls import include, url
+# from django.contrib import admin
+#
+# urlpatterns = [
+#     url(r'^admin/', include(admin.site.urls)),
+#     url(r'', include('blog.urls')),
+# ]
